7ab257fb1330e9ab09693a03baee3d4eb5c25a49
Shubaa29 <georgiuscoc69@gmail.com>
2025-03-21 22:48:30 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,19 +1,15 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+ import matplotlib.pyplot as plt
+import seaborn as sns
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.preprocessing import LabelEncoder, StandardScaler
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 
-# Check if matplotlib and seaborn are installed
-try:
-    import matplotlib.pyplot as plt
-    import seaborn as sns
-    st.write("Matplotlib and Seaborn are installed and working!")
-except ImportError as e:
-    st.error(f"Error importing matplotlib or seaborn: {e}")
-    st.stop()
+
+ st.write("This App Will Predict Your Obesity level!")
 
 class ObesityPredictionApp:
     def __init__(self, data):
