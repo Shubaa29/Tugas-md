faf94d266482530866bc1edff8483bbc93ea9139
Shubaa29 <georgiuscoc69@gmail.com>
2025-03-21 18:14:26 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler, LabelEncoder
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import accuracy_score, classification_report
+
 
 class ObesityModel:
     def __init__(self, data_path):
