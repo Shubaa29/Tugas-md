0c13c4e48368d7336cd7bf6a7ed7f0ff68e9d486
Shubaa29 <georgiuscoc69@gmail.com>
2025-03-21 22:48:54 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,14 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
- import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.preprocessing import LabelEncoder, StandardScaler
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 
-
- st.write("This App Will Predict Your Obesity level!")
+st.write("This App Will Predict Your Obesity level!")
 
 class ObesityPredictionApp:
     def __init__(self, data):
